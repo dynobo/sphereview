24f7e9e7b72efd6fc34067149b14ff50980b1388
dynobo <dynobo@mailbox.org>
2025-07-02 17:14:57 +0200

    chore: update version

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod image;
 mod shortcuts_dialog;
 mod window;
 
-pub const APP_VERSION: &str = "0.1.1";
+pub const APP_VERSION: &str = "0.1.2";
 pub const APP_ID: &str = "io.github.dynobo.sphereview";
 
 use app::App;
