f69b69f784b0eea0bbbf44ff37ff9c2da98edb90
dynobo <dynobo@mailbox.org>
2025-07-04 20:37:07 +0200

    tests: add version consistency test

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,8 @@ mod image;
 mod shortcuts_dialog;
 mod window;
 
-pub const APP_VERSION: &str = "0.1.2";
-pub const APP_ID: &str = "io.github.dynobo.sphereview";
-
 use app::App;
+use sphereview::{APP_ID, APP_VERSION};
 
 fn main() {
     gtk4::init().expect("Error initializing gtk.");
